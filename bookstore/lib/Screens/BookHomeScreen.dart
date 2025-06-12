ec8fdb6a7ab262d1e360b2d956ffbb14c91c500d
Sparsh261 <sparshchauhan112@gmail.com>
2025-06-12 13:55:48 +0530

    Added HOme page and model

---
+++ bookstore/lib/Screens/BookHomeScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+
+class BookHomeScreen extends StatefulWidget{
+  @override
+  State<BookHomeScreen> createState() => _BookHomeScreenState();
+}
+
+class _BookHomeScreenState extends State<BookHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ bookstore/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:bookstore/Screens/BookHomeScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: BookHomeScreen(),));
+}
